f0d8bbd956a36b5107db26add6ef2e6dcdf1b99e
yangyuan <yangyuan501x@gmail.com>
2022-04-08 17:10:54 +0800

    0408

---
+++ json test.py
@@ -0,0 +1,25 @@
+import jsonlines
+#with open("test.json", "r",encoding='UTF-8') as read_content:
+ #   print(json.load(read_content))
+fp = open('temp.json', 'w', encoding='utf-8')
+#json_data=[]
+i=1
+fp = open('temp.json', 'w', encoding='utf-8')
+for line in open('CET4luan_1.json', 'r', encoding='utf-8'):
+    if(i==1):
+        if(str(line).strip() != '"bookId": "CET4luan_1"'):
+            fp.write(str(line).strip())
+
+        else:
+            fp.write(str(line).strip())
+            fp.write('}')
+            fp.write('\n')
+            i=0
+    else:
+        i=1
+fp.close()
+
+with open('temp.json', 'r',encoding='utf-8') as f:
+    for item in jsonlines.Reader(f):
+
+        print(item)
