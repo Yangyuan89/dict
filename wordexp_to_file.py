724f12dcd6e484d2ba37655c4720037d15613b48
yangyuan <yangyuan501x@gmail.com>
2022-04-11 18:21:27 +0800

    0411

--- wordexp_to_file.py
+++ wordexp_to_file.py
@@ -7,33 +7,57 @@ import jsonlines
 
 word = []
 with open('1.json', 'r',encoding='utf-8') as f:
+    word_file = open("四级英语词汇导出.xlsx", 'w', encoding='UTF-8')
     for item in jsonlines.Reader(f):
-        print(item)
+        #print(item)
         #输出单词
         print(item['word'])
-
+        word_file.write(item['word'])
+        word_file.write('\t')
         #输出音标
         if(len(item['phonetic_list'])==4):
          print(item['phonetic_list'][0]+item['phonetic_list'][1]+'  '+item['phonetic_list'][2]+item['phonetic_list'][3])
+         word_file.write(item['phonetic_list'][0]+item['phonetic_list'][1]+'  '+item['phonetic_list'][2]+item['phonetic_list'][3])
+         word_file.write('\t')
         elif (len(item['phonetic_list']) == 2):
-            print(item['phonetic_list'][0] + item['phonetic_list'][1] )
+            print(item['phonetic_list'][0] + item['phonetic_list'][1])
+            word_file.write(item['phonetic_list'][0] + item['phonetic_list'][1])
+            word_file.write('\t')
         else:
             print('')
+            word_file.write('\t')
+            word_file.write('\t')
         #输出单词简单翻译
         print(''.join(item['word_trans_simple']))
-
+        word_file.write(''.join(item['word_trans_simple']))
+        word_file.write('\t')
         #输出单词朗读文本
         print(''.join(item['word_trans_read']))
-
+        word_file.write(''.join(item['word_trans_read']))
+        word_file.write('\t')
 
         # 输出例句英文
         if(len(item['liju_list'][1])):
             print(item['liju_list'][1][0])
+            word_file.write(item['liju_list'][1][0])
+            word_file.write('\t')
             #输出例句中文
             print(item['liju_list'][0][0])
+            word_file.write(item['liju_list'][0][0])
+            word_file.write('\t')
 
-
-
+        # 输出短语英文
+        if (len(item['wordGroup_list'][1])):
+            print(item['wordGroup_list'][0][0])
+            word_file.write(item['wordGroup_list'][0][0])
+            word_file.write('\t')
+            # 输出例句中文
+            print(item['wordGroup_list'][1][0])
+            word_file.write(item['wordGroup_list'][1][0])
+            word_file.write('\t')
+        word_file.write('\n')
+    word_file.close()
+    f.close()
 
 
 
